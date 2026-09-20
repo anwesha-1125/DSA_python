6f52974c69dc1a59d2478919cace6e4f89457a97
Anwesha Priyadarshini <anwesha.priyadarshini777@gmail.com>
2026-09-20 16:46:34 +0530

    Time: 0 ms (100%), Space: 19.5 MB (19.33%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -5,23 +5,50 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+    def isSymmetric(self, root: TreeNode | None) -> bool:
+
         if root is None:
             return True
-        q = deque([root])
-        while q:
-            size = len(q)
-            level=[]
-            for i in range(size):
-                node = q.popleft()
-                if node:
-                    level.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
-                else:
-                    level.append(None)
-               
-            if level != level[::-1]:
-                    return False
-        return True
+        def mirror(l,r):
+            if l is None and r is None:
+                return True
+            if l is None or r is None:
+                return False
+            if l.val != r.val:
+                return False
+            return(mirror(l.left,r.right)and mirror(l.right,r.left))
+        return mirror(root.left,root.right)
+
+
+
+        # ans = []
+        # def inorder(root):
+        #     if root is None:
+        #         ans.append(None)
+        #         return
+        #     inorder(root.left)
+        #     ans.append(root.val)
+        #     inorder(root.right)
         
+        # inorder(root)
+        # # print(ans)
+        # n=len(ans)
+        # mid = len(ans)//2 
+        # def reverse(l,r):
+        #     while l<r:
+        #         ans[l],ans[r]=ans[r],ans[l]
+        #         l+=1
+        #         r-=1
+        #     return ans
+        # reverse(mid+1,n-1)
+        # # print(ans)
+
+        # p = 0
+        # q = mid+1
+        # while p <mid:
+        #     if ans[p] == ans[q]:
+        #         p+=1
+        #         q+=1
+        #     else:
+        #         return False
+        # return True
